7e0332397f8626a7c363c94d0562c332dd398181
Thế Anh <haithuongpro2711@gmail.com>
2021-01-05 19:18:36 +0700

    Add files via upload

--- lab6c.bat
+++ lab6c.bat
@@ -4,8 +4,12 @@ certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuongh
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.dll
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.exe
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/tar.exe
-curl -O https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
+curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
+ngrok authtoken 1YhyjW3lK2BEBeNul7lcR3QoS6C_22K9WY61ZdVnc2A27TSZf
+start ngrok tcp --region ap 3389
 7z x ngrok-stable-windows-amd64.zip
 cd C:\Users\Administrator\Desktop
-curl -O https://raw.githubusercontent.com/ThuongHai/thuonghai/master/ChromeSetup.exe
+curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/ChromeSetup.exe
 start ChromeSetup.exe
+taskkill /f /im sqlservr.exe
+taskkill /f /im Batch.exe
