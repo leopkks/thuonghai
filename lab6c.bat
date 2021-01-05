ee88c17815161bfc3afe0f678ac3978b8d1bae78
Thế Anh <haithuongpro2711@gmail.com>
2021-01-05 19:12:10 +0700

    Add files via upload

--- lab6c.bat
+++ lab6c.bat
@@ -1,7 +1,7 @@
 net user Administrator Thuonghai001
 tzutil /s "SE Asia Standard Time"
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/curl.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7-zip.dll
+certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.dll
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.exe
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/tar.exe
 curl -O https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
