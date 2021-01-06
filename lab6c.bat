a873ab8a3929b8d233b1dad23fdf3b3ccd22306f
Thế Anh <haithuongpro2711@gmail.com>
2021-01-06 11:05:14 +0700

    Add files via upload

--- lab6c.bat
+++ lab6c.bat
@@ -3,11 +3,10 @@ tzutil /s "SE Asia Standard Time"
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/curl.exe
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.dll
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/tar.exe
 curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
 7z x ngrok-stable-windows-amd64.zip
 ngrok authtoken 1YhyjW3lK2BEBeNul7lcR3QoS6C_22K9WY61ZdVnc2A27TSZf
-start ngrok tcp --region ap 3389
+start ngrok tcp 3389
 cd C:\Users\Administrator\Desktop
 curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/ChromeSetup.exe
 start ChromeSetup.exe
