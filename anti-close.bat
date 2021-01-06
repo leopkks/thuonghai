dcefd5dac8d4c5ec492f95145dd7bc7037016866
Thế Anh <haithuongpro2711@gmail.com>
2021-01-06 17:56:22 +0700

    Add files via upload

--- anti-close.bat
+++ anti-close.bat
@@ -6,8 +6,9 @@ cd C:\Program Files (x86)\Internet Explorer
 start iexplore.exe  https://labondemand.com/LabInstance/ResumeWithKey/?key=%id%
 timeout 1
 nircmd.exe win setsize process iexplore.exe 0 0 1 1
-timeout 5
-taskkill /f /im iexplore.exe
+timeout 1
+nircmd.exe win min process iexplore.exe
 timeout 300
+taskkill /f /im iexplore.exe
 cls
 goto loop
