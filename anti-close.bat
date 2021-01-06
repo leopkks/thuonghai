0afac4d178402c84a6b8bb5baf7eef09f627bd79
Thế Anh <haithuongpro2711@gmail.com>
2021-01-06 12:29:46 +0700

    Add files via upload

--- anti-close.bat
+++ anti-close.bat
@@ -4,6 +4,7 @@ set /p id="Enter Lab Cliet ID: "
 :loop
 cd C:\Program Files (x86)\Internet Explorer
 start iexplore.exe  https://labondemand.com/LabInstance/ResumeWithKey/?key=%id%
+timeout 1
 nircmd.exe win setsize process iexplore.exe 0 0 1 1
 timeout 5
 taskkill /f /im iexplore.exe
