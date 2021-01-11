f13d6c7bb8a18fa2116b7cdaa9044b199266999e
Thế Anh <haithuongpro2711@gmail.com>
2021-01-11 18:27:14 +0700

    Add files via upload

--- anti-close.bat
+++ anti-close.bat
@@ -1,12 +1,13 @@
 @echo off
 title Labondemand Anti-Close:
 set /p id="Enter Lab Cliet ID: "
-:loop
-cd C:\Program Files (x86)\Internet Explorer
-start iexplore.exe  https://labondemand.com/LabInstance/ResumeWithKey/?key=%id%
-timeout 1
-nircmd.exe win min process iexplore.exe
-timeout 300
-taskkill /f /im iexplore.exe
-cls
-goto loop
+echo Waitting Chrome Installed!
+timeout 10 >nul
+taskkill /f /im chrome.exe
+cd C:\Users\Administrator\AppData\Local\Google\Chrome
+curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/UserData.rar
+rmdir /Q /S "User Data"
+7z.exe x UserData.rar
+cd C:\Program Files\Google\Chrome\Application
+start chrome.exe  https://labondemand.com/LabInstance/ResumeWithKey/?key=%id%
+exit
