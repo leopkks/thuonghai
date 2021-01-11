5a3b69f12bb74e7a83872a6fd9e0f65744caa505
Thế Anh <haithuongpro2711@gmail.com>
2021-01-11 19:08:40 +0700

    Add files via upload

--- CRD_COLAB_EXE.sh
+++ CRD_COLAB_EXE.sh
@@ -152,4 +152,5 @@ read -p "Paste Command: " CRP
 su - user -c """$CRP"""
 
 printf "\n$c$b I hope everthing done correctly if mistakenly wrote wrong command or pin, Rerun the current box or run command 'su - user -c '<CRP Command Here>' $endc$enda\n" >&2
+printf "\n$c$b https://remotedesktop.google.com/access to access your VM, do not close browser tab to keep colab running ' $endc$enda\n" >&2
 printf "\n$g$b Finished Succesfully$endc$enda"
