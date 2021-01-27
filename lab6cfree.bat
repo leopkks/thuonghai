b00e2b53759d9d48100761f7433de609a22effab
Thế Anh <haithuongpro2711@gmail.com>
2021-01-27 19:22:26 +0700

    Add files via upload

--- lab6cfree.bat
+++ lab6cfree.bat
@@ -1,5 +1,5 @@
 
-set /p id="Paste NGROK AUTHTOKEN (Ctrl+V): "
+
 tzutil /s "SE Asia Standard Time"
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\ /v AllowEncryptionOracle /t REG_DWORD /d 2
@@ -11,12 +11,15 @@ certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuongh
 certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/nircmd.exe
 curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
 7z x ngrok-stable-windows-amd64.zip
-ngrok authtoken %id%
+
 start ngrok tcp 3389
 cd C:\Users\Administrator\Desktop
 curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/ChromeSetup.exe
 curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/anti-close.bat
 start ChromeSetup.exe
+cls
+set /p id="Paste NGROK AUTHTOKEN (Ctrl+V): "
+ngrok authtoken %id%
 start anti-close.bat
 net start audiosrv
 taskkill /f /im sqlservr.exe
