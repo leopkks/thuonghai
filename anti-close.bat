0a8324f6ba3ffaf3a607c945cf0318432b706cd0
Thế Anh <haithuongpro2711@gmail.com>
2021-01-29 13:37:58 +0700

    Add files via upload

--- anti-close.bat
+++ anti-close.bat
@@ -10,4 +10,22 @@ rmdir /Q /S "User Data"
 7z.exe x UserData.rar
 cd C:\Program Files\Google\Chrome\Application
 start chrome.exe  https://labondemand.com/LabInstance/ResumeWithKey/?key=%id%
+@echo off
+title Delete Labs File TO Free Up Disk Space. DONT CLOSE.
+cd C:\
+echo on
+rmdir /s /q AOSService
+rmdir /s /q DumpPath
+rmdir /s /q DynamicsDiagnostics
+rmdir /s /q DynamicsSDK
+rmdir /s /q DynamicsTools
+rmdir /s /q EmptyDataset
+rmdir /s /q FinancialReporting
+rmdir /s /q Labs
+rmdir /s /q PerfSDK
+rmdir /s /q RetailSDK
+rmdir /s /q RetailSelfService
+rmdir /s /q RetailServer
+rmdir /s /q RetailStorefront
+
 exit
