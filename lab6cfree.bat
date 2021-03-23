533ea76c34837e679e3c24348360019e226909da
leopkks <leopkks@gmail.com>
2021-03-23 17:48:06 -0300

    Update lab6cfree.bat

--- lab6cfree.bat
+++ lab6cfree.bat
@@ -5,17 +5,17 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v Dis
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System\CredSSP\Parameters\ /v AllowEncryptionOracle /t REG_DWORD /d 2
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\DWM" /v ColorPrevalence /t REG_DWORD /d 1 /f
 Reg Add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v ColorPrevalence /t REG_DWORD /d 1 /f
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/curl.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.dll
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/7z.exe
-certutil -urlcache -split -f https://raw.githubusercontent.com/ThuongHai/thuonghai/master/nircmd.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/leopkks/thuonghai/master/curl.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/leopkks/thuonghai/master/7z.dll
+certutil -urlcache -split -f https://raw.githubusercontent.com/leopkks/thuonghai/master/7z.exe
+certutil -urlcache -split -f https://raw.githubusercontent.com/leopkks/thuonghai/master/nircmd.exe
 curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip
 7z x ngrok-stable-windows-amd64.zip
 
 
 cd C:\Users\Administrator\Desktop
-curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/ChromeSetup.exe
-curl -LJOk https://raw.githubusercontent.com/ThuongHai/thuonghai/master/anti-close.bat
+curl -LJOk https://raw.githubusercontent.com/leopkks/thuonghai/master/ChromeSetup.exe
+curl -LJOk https://raw.githubusercontent.com/leopkks/thuonghai/master/anti-close.bat
 start ChromeSetup.exe
 cls
 @echo off
